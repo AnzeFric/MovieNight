56d5f20e7e842da580da5924dc775a513eb40163
AnzeFric <anzeifric@gmail.com>
2025-07-14 10:40:38 +0200

    Mobile: Fixed tabs for correct display

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,15 +1,43 @@
-import { Colors } from "@/constants/Colors";
 import { Ionicons } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
-import React from "react";
+import { Pressable } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function TabLayout() {
+  const insets = useSafeAreaInsets();
+
   return (
     <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors.dark.specialBlue,
+      screenOptions={() => ({
         headerShown: false,
-      }}
+        tabBarShowLabel: false,
+        sceneStyle: {
+          paddingTop: insets.top,
+        },
+
+        // Disables android default onClick ripple effect
+        tabBarButton: (props) => (
+          <Pressable
+            onPress={props.onPress}
+            onLongPress={props.onLongPress}
+            testID={props.testID}
+            accessibilityLabel={props.accessibilityLabel}
+            accessibilityRole={props.accessibilityRole}
+            accessibilityState={props.accessibilityState}
+            style={[
+              props.style,
+              {
+                alignItems: "center",
+                justifyContent: "center",
+              },
+            ]}
+            android_ripple={{ borderless: true, color: "transparent" }}
+          >
+            {props.children}
+          </Pressable>
+        ),
+      })}
+      backBehavior="history"
     >
       <Tabs.Screen
         name="index"
