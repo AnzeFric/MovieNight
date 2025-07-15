91601f3603bb9af3c422d199b87ad2bf19e0f35a
AnzeFric <anzeifric@gmail.com>
2025-07-15 21:03:30 +0200

    Mobile: Fixed soft keyboard hide content on text input focus and scrollview not working properly

--- app/(tabs)/watchlist/add.tsx
+++ app/(tabs)/watchlist/add.tsx
@@ -3,7 +3,12 @@ import AddMovieForm from "@/components/watchlist/AddMovieForm";
 import { MovieInfo } from "@/interfaces/movie";
 import { useFocusEffect } from "expo-router";
 import { useCallback, useState } from "react";
-import { ScrollView, StyleSheet } from "react-native";
+import {
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  StyleSheet,
+} from "react-native";
 
 const defaultMovieInfo: MovieInfo = {
   name: "",
@@ -22,19 +27,33 @@ export default function AddMovie() {
   );
 
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-      <CustomText type={"lTitle"} bold>
-        Add a movie
-      </CustomText>
-      <AddMovieForm setMovie={setMovie} />
-    </ScrollView>
+    <KeyboardAvoidingView
+      style={styles.keyboardContainer}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      keyboardVerticalOffset={Platform.OS === "ios" ? 150 : 50}
+    >
+      <ScrollView
+        contentContainerStyle={styles.container}
+        keyboardShouldPersistTaps="handled"
+        showsVerticalScrollIndicator={false}
+      >
+        <CustomText type={"lTitle"} bold>
+          Add a movie
+        </CustomText>
+        <AddMovieForm setMovie={setMovie} />
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
+  keyboardContainer: {
+    flex: 1,
+  },
   container: {
     paddingHorizontal: 25,
     paddingVertical: 20,
     gap: 20,
+    flexGrow: 1,
   },
 });
