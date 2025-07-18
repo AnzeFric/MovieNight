3a9e333885178207f6b89d64ebbccef73eb317c9
AnzeFric <anzeifric@gmail.com>
2025-07-18 11:28:59 +0200

    Mobile: Added createMovie function to hook

--- hooks/useMovies.ts
+++ hooks/useMovies.ts
@@ -1,4 +1,5 @@
 import { MovieService } from "@/database/services/MovieService";
+import { MovieInfo } from "@/interfaces/movie";
 import { Alert } from "react-native";
 
 export function useMovies() {
@@ -7,7 +8,7 @@ export function useMovies() {
       const movies = await MovieService.fetchMovies();
       return movies;
     } catch (error) {
-      console.error("Error while fetching movies: ", error);
+      console.error("Error while fetching data: ", error);
       Alert.alert(
         "Error",
         "An error occured while fetching data. Try again later."
@@ -15,7 +16,21 @@ export function useMovies() {
     }
   };
 
+  const createMovie = async (movieData: MovieInfo) => {
+    try {
+      const movieId = await MovieService.createMovie(movieData);
+      return movieId ? true : false;
+    } catch (error) {
+      console.error("Error while saving data: ", error);
+      Alert.alert(
+        "Error",
+        "An error occured while saving data. Try again later."
+      );
+    }
+  };
+
   return {
     fetchMovies,
+    createMovie,
   };
 }
